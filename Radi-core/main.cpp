c51cbfbe5d6d3ea841a17e5a1225d8e0a8065894
RadiatorTwo <radiatortwo@gmail.com>
2016-09-15 21:33:29 +0200

    Matrix mat4 added

--- Radi-core/main.cpp
+++ Radi-core/main.cpp
@@ -12,7 +12,10 @@ int main()
 	Window window("Radi!", 800, 600);
 
 	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
-	
+
+	mat4 position = mat4::translation(vec3(2, 3, 4));
+	position *= mat4::identity();
+
 	while (!window.closed())
 	{
 		window.clear();
